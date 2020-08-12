d8ecb8d552aae48bb416d60e8698fa20d61b0d4e
Jody Ingram <SphericalCrusher@users.noreply.github.com>
2020-08-11 22:53:51 -0400

    Create SCCM-Query-No-Client.ps1

---
+++ SCCM-Query-No-Client.ps1
@@ -0,0 +1,14 @@
+# Microsoft System Center Configuration Manager SCCM Client Detector
+#------------------------------------------------------------
+# This will report on machines that do not have the SCCM client installed or is corrupt.
+#------------------------------------------------------------
+
+# Import SCCM PowerShell Module. Change file path if install location is different.
+
+Import-Module ‘C:\Program Files (x86)\Microsoft Configuration Manager\AdminConsole\bin\ConfigurationManager.psd1’
+$CMSite=”$(Get-PSDrive –PSProvider CMSite)`:”
+Set-Location $CMSite
+
+# Query. Can also be ran directly in SCCM Device Collections.
+
+select SMS_R_System.ResourceId, SMS_R_System.ResourceType, SMS_R_System.Name, SMS_R_System.SMSUniqueIdentifier, SMS_R_System.ResourceDomainORWorkgroup, SMS_R_System.Client from  SMS_R_System where SMS_R_System.Client = "0"
