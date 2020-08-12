85d63df5d90e3545d9c9adcf3101c3c5f0630f78
Jody Ingram <SphericalCrusher@users.noreply.github.com>
2020-08-11 21:25:32 -0400

    Create Search-Exchange-Mailbox.ps1

---
+++ Search-Exchange-Mailbox.ps1
@@ -0,0 +1,19 @@
+# Search Exchange Mailbox
+# This script searches all Exchange mailboxes for specific criteria and copies the e-mail out to another user's mailbox.
+
+# USAGE INSTRUCTIONS
+#-----------------------------------------------------------------------------------------------------------------------
+# Change Subject:"THESE WORDS" to whatever Subject string you want to lookup. Add a comma ";" between subjects to use multiple at once. 
+# Changed "And Received" date to the before and after dates you want to search for
+# Target MailBox is where the e-mails will be copied to. Target Folder is which Inbox folder it copies to.
+# Log Level "Full" adds a complete list of search results. Can be turned off by changing "Full" to "Suppress"
+#-----------------------------------------------------------------------------------------------------------------------
+
+#Run ADd-PSSnapIn to add Exchange PowerShell Modules
+Add-PSsnapin Microsoft.Exchange.Management.PowerShell.E2010
+
+#Customize the below script as you see fit to modify results
+
+Get-Mailbox | Search-Mailbox -SearchQuery {Subject:"Your account is being used by another person!; The decision to suspend your account. Waiting for payment.; High danger. Your account was attacked.; Your account was attacked; High alert. Your account was attacked" And Received:04/14/2019..04/15/2019} -targetmailbox "jody.ingram" -targetfolder "Phish" -loglevel "full"
+
+# Add " -DeleteContent " to the end to delete the items from user's mailbox
