45bc64567f91a1d7e2ff69c2dbc7cba59adf7654
Jody Ingram <SphericalCrusher@users.noreply.github.com>
2020-08-11 21:36:00 -0400

    Create SCCM-SMB-Detector.ps1

---
+++ SCCM-SMB-Detector.ps1
@@ -0,0 +1,24 @@
+# Microsoft System Center Configuration Manager SMB Detection Query
+#------------------------------------------------------------
+# Author: Jody Ingram
+#------------------------------------------------------------
+# To change SMB version, change the "%SMB2%" to SMB1 or SMB3. 
+#------------------------------------------------------------
+
+# Import SCCM PowerShell Module. Change file path if install location is different.
+
+Import-Module ‘C:\Program Files (x86)\Microsoft Configuration Manager\AdminConsole\bin\ConfigurationManager.psd1’
+$CMSite=”$(Get-PSDrive –PSProvider CMSite)`:”
+Set-Location $CMSite
+
+select 
+
+SMS_R_SYSTEM.ResourceID,SMS_R_SYSTEM.ResourceType,SMS_R_SYSTEM.Name,SMS_R_SYSTEM.SMSUniqueIdentifier,SMS_R_SYSTEM.Resource
+
+DomainORWorkgroup,SMS_R_SYSTEM.Client from SMS_R_System inner join SMS_G_System_ADD_REMOVE_PROGRAMS on 
+
+SMS_G_System_ADD_REMOVE_PROGRAMS.ResourceId = SMS_R_System.ResourceId where SMS_G_System_ADD_REMOVE_PROGRAMS.DisplayName 
+
+like "%SMB2%"
+
+Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters | ForEach-Object {Get-ItemProperty $_.pspath}
