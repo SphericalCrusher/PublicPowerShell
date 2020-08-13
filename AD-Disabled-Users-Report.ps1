284d3422813cc4addd601c05eb1a68e146708514
Jody Ingram <SphericalCrusher@users.noreply.github.com>
2020-08-12 22:54:17 -0400

    Update AD-Disabled-Users-Report.ps1

--- AD-Disabled-Users-Report.ps1
+++ AD-Disabled-Users-Report.ps1
@@ -1,9 +1,15 @@
-# Disabled AD Users Report
-# Exports a list of users currently disabled in Active Directory.
+<#
+Script  :  AD-Disabled-Users-Report.ps1
+Version :  1.0
+Date    :  11/16/2015
+Author: Jody Ingram
+Pre-reqs: N/A
+Notes: This will export a list of users that are currently disabled in Active Directory to a .csv file.
+#>
 
 import-module ActiveDirectory
 $date = Get-Date -Format "MM_dd_yyyy_HH_mm"
 $name = "Disabled_Accounts_Report_$date"
 Search-ADAccount -AccountDisabled | Select-Object Name >> \\SERVER\FILEPATH\$name.csv
-"The Report of disabled user has been placed in \\SERVER\FILEPATH" 
+"The Report of disabled users has been placed in \\SERVER\FILEPATH" 
 cmd /c pause | out-null
