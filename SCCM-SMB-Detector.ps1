9259f6367badd094286a1680fc93e817db3c3bd4
Jody Ingram <SphericalCrusher@users.noreply.github.com>
2020-08-17 15:34:06 -0400

    Update SCCM-SMB-Detector.ps1

--- SCCM-SMB-Detector.ps1
+++ SCCM-SMB-Detector.ps1
@@ -1,9 +1,11 @@
-# Microsoft System Center Configuration Manager SMB Detection Query
-#------------------------------------------------------------
-# Author: Jody Ingram
-#------------------------------------------------------------
-# To change SMB version, change the "%SMB2%" to SMB1 or SMB3. 
-#------------------------------------------------------------
+<#
+Script  :  SCCM-SMB-Detector.ps1
+Version :  1.0
+Date    :  11/16/2016
+Author: Jody Ingram
+Pre-reqs: SCCM PowerShell Module or run on a server with SCCM console installed.
+Notes: Microsoft System Center Configuration Manager SMB Detection Query. To change SMB version, change the "%SMB2%" to SMB1 or SMB3.
+#>
 
 # Import SCCM PowerShell Module. Change file path if install location is different.
 
