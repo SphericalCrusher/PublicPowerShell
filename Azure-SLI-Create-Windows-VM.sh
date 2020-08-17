1d10ac8b620d146eb82420ca43fe56e9e4299086
Jody Ingram <SphericalCrusher@users.noreply.github.com>
2020-08-17 15:19:24 -0400

    Update Azure-SLI-Create-Windows-VM.sh

--- Azure-SLI-Create-Windows-VM.sh
+++ Azure-SLI-Create-Windows-VM.sh
@@ -16,7 +16,7 @@ $vmObj1 = New-AzureVM -Name $vm1 -InstanceSize Small -ImageName $img
 
 $vmObj1 = Add-AzureProvisioning -VM $vmObj1 -Password $password -AdminUsername $username -Windows
 
-# Enables Chef extension to Azure
+# Enables Chef extension for Azure
 
 $vmObj1 = Set-AzureVMChefExtension -VM $vmObj1 -ValidationPem "C:\users\username\msazurechef-validator.pem" -ClientRb
 "C:\users\username\client.rb" -RunList "getting-started" -Windows
