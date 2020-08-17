c53a725a4bd01b79caa2f545b8ca2acbcead5570
Jody Ingram <SphericalCrusher@users.noreply.github.com>
2020-08-17 15:26:02 -0400

    Update Exchange-Set-AutoResponder.ps1

--- Exchange-Set-AutoResponder.ps1
+++ Exchange-Set-AutoResponder.ps1
@@ -1,8 +1,15 @@
-#Set Outlook Auto Responder Remotely 
+<#
+Script  :  Exchange-Set-AutoResponder.ps1
+Version :  1.0
+Date    :  11/16/2016
+Author: Jody Ingram
+Pre-reqs: N/A
+Notes: This script set a user's Outlook Auto Responder Remotely 
+#>
 
-#Run Add-PSSnapIn to add Exchange PowerShell Modules
+# Run Add-PSSnapIn to add Exchange PowerShell Modules
 Add-PSsnapin Microsoft.Exchange.Management.PowerShell.E2010
 
-#Modify below configuration as needed.
+# Modify below configuration as needed.
 
 Set-MailboxAutoReplyConfiguration -Identity USERNAME -AutoReplyState Enabled -InternalMessage "Internal auto-reply message." -ExternalMessage "External auto-reply message."
