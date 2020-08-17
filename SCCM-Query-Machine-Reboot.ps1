1052646c456389785c2e7f56f905fd2934dab26c
Jody Ingram <SphericalCrusher@users.noreply.github.com>
2020-08-17 16:07:55 -0400

    Update SCCM-Query-Machine-Reboot.ps1

--- SCCM-Query-Machine-Reboot.ps1
+++ SCCM-Query-Machine-Reboot.ps1
@@ -13,10 +13,11 @@ Import-Module ‘C:\Program Files (x86)\Microsoft Configuration Manager\AdminCon
 $CMSite=”$(Get-PSDrive –PSProvider CMSite)`:”
 Set-Location $CMSite
 
-# Query. Can also be ran directly in SCCM Device Collections. 
+# Start Query. Can also be ran directly in SCCM Device Collections. 
 
 select SMS_R_SYSTEM.ResourceID,SMS_R_SYSTEM.ResourceType,SMS_R_SYSTEM.Name,
  SMS_R_SYSTEM.SMSUniqueIdentifier,SMS_R_SYSTEM.ResourceDomainORWorkgroup,
  SMS_R_SYSTEM.Client from SMS_R_System join sms_combineddeviceresources on
  sms_combineddeviceresources.resourceid = sms_r_system.resourceid
  where sms_combineddeviceresources.clientstate != 0
+# End Query
