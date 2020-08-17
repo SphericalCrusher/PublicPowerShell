86e8a137ffc5d9e8a9c10b42875ac45c8e3de338
Jody Ingram <SphericalCrusher@users.noreply.github.com>
2020-08-17 15:45:53 -0400

    Update Windows-File-Server-Cleanup.ps1

--- Windows-File-Server-Cleanup.ps1
+++ Windows-File-Server-Cleanup.ps1
@@ -1,4 +1,11 @@
-# This script will delete all files in the network folders older than <Specified Days>. The time can be adjusted below.
+<#
+Script  :  Windows-File-Server-Cleanup.ps1
+Version :  1.0
+Date    :  11/16/2018
+Author: Jody Ingram
+Pre-reqs: N/A
+Notes: This script will delete all files in the network folders older than <Specified Days>. The time can be adjusted below.
+#>
 
 $Path = “LOCATION HERE”
 $Daysback = “-1”
