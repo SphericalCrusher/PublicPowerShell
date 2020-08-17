bd2a8356191ca8a5083562cd1e8ea5589d512d09
Jody Ingram <SphericalCrusher@users.noreply.github.com>
2020-08-17 15:38:58 -0400

    Update SCCM-WSUS-Cleanup.ps1

--- SCCM-WSUS-Cleanup.ps1
+++ SCCM-WSUS-Cleanup.ps1
@@ -1,5 +1,11 @@
-# This script is used to cleanup WSUS for SCCM.
-# This cleans up expired and superseded Windows updates from the WSUS server.
+<#
+Script  :  SCCM-WSUS-Cleanup.ps1
+Version :  1.0
+Date    :  11/16/2014
+Author: Jody Ingram
+Pre-reqs: SCCM PowerShell Module or run on a server with SCCM & WSUS installed.
+Notes: This script is used to cleanup WSUS for SCCM. This cleans up expired and superseded Windows updates from the WSUS server.
+#>
 
 Write-Host -Foreground Red "Type WSUS-Cleanup to begin!"
 function WSUS-Cleanup(){
@@ -29,9 +35,6 @@ Write-Host "Failed to run. Please verify you have run PowerShell as Administrato
 }
 
 }
-
-
-#---------------------------------------------
 
 Write-Host -Foreground Red "Type WSUS-Cleanup to start!"
 function WSUS-Cleanup(){
