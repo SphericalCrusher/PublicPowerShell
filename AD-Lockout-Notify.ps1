dffa8146f46cba3faaef139efb391f04c14ffe5d
Jody Ingram <SphericalCrusher@users.noreply.github.com>
2020-08-17 15:57:24 -0400

    Update AD-Lockout-Notify.ps1

--- AD-Lockout-Notify.ps1
+++ AD-Lockout-Notify.ps1
@@ -5,6 +5,17 @@ Date    :  1/16/2019
 Author: Jody Ingram
 Pre-reqs: N/A
 Notes: This script sends an e-mail when any Active Directory Account gets locked out. It can also automatically unlock a specific username. Run instructions are below.
+
+INSTRUCTIONS:
+
+To run this script, you will need to create a scheduled task:
+
+Triggers: On an event - On event - Log: Security, Source: Microsoft Windows Security Auditing, EventID: 4740
+
+Action: PowerShell.exe -nologo -File "C:\TEMP\AD-Lockout-Notify.ps1 (Location of this script)
+
+Click the "Run with highest privileges" box.
+
 #>
 
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
@@ -24,17 +35,3 @@ Send-MailMessage @messageParameters
 # This can be used to automatically unlock a user in a worst case scenario
 
 Unlock-ADAccount -identity USERNAME
-
-<#
-
-INSTRUCTIONS
-
-To run this script, you will need to create a scheduled task:
-
-Triggers: On an event - On event - Log: Security, Source: Microsoft Windows Security Auditing, EventID: 4740
-
-Action: PowerShell.exe -nologo -File "C:\TEMP\AD-Lockout-Notify.ps1 (Location of this script)
-
-Click the "Run with highest privileges" box.
-
-#>
