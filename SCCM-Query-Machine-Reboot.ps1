9b79f13cd4b7bcfe25c036c990de38fac27023ed
Jody Ingram <SphericalCrusher@users.noreply.github.com>
2020-08-17 15:31:29 -0400

    Update SCCM-Query-Machine-Reboot.ps1

--- SCCM-Query-Machine-Reboot.ps1
+++ SCCM-Query-Machine-Reboot.ps1
@@ -1,7 +1,11 @@
-# Microsoft System Center Configuration Manager Reboot Query
-#------------------------------------------------------------
-# This will report on machines that require a restart after updates were successfully installed.
-#------------------------------------------------------------
+<#
+Script  :  SCCM-Query-Machine-Reboot.ps1
+Version :  1.0
+Date    :  11/16/2016
+Author: Jody Ingram
+Pre-reqs: SCCM PowerShell Module or run on a server with SCCM console installed.
+Notes: Microsoft System Center Configuration Manager Reboot Query. This will report on machines that require a restart after updates were successfully installed.
+#>
 
 # Import SCCM PowerShell Module. Change file path if install location is different.
 
