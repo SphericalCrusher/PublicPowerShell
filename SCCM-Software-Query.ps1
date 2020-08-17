68a6676daa4dbfeffb8f7e6dc7cd5b2b1e97b0cc
Jody Ingram <SphericalCrusher@users.noreply.github.com>
2020-08-17 16:09:59 -0400

    Update SCCM-Software-Query.ps1

--- SCCM-Software-Query.ps1
+++ SCCM-Software-Query.ps1
@@ -14,6 +14,5 @@ $CMSite=”$(Get-PSDrive –PSProvider CMSite)`:”
 Set-Location $CMSite
 
 # Start Query. Can also be ran directly in SCCM Device Collections. 
-
 select SMS_R_SYSTEM.ResourceID,SMS_R_SYSTEM.ResourceType,SMS_R_SYSTEM.Name,SMS_R_SYSTEM.SMSUniqueIdentifier,SMS_R_SYSTEM.ResourceDomainORWorkgroup,SMS_R_SYSTEM.Client from SMS_R_System inner join SMS_G_System_ADD_REMOVE_PROGRAMS on SMS_G_System_ADD_REMOVE_PROGRAMS.ResourceId = SMS_R_System.ResourceId where SMS_G_System_ADD_REMOVE_PROGRAMS.DisplayName like "%APPLICATION%"
 # End Query
