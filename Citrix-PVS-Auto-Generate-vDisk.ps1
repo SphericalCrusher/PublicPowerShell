60935aa75c58f17fb9094864aa1b216a02054a90
Jody Ingram <22178885+Jody-Ingram@users.noreply.github.com>
2022-03-24 10:36:06 -0400

    Update Citrix-PVS-Auto-Generate-vDisk.ps1

--- Citrix-PVS-Auto-Generate-vDisk.ps1
+++ Citrix-PVS-Auto-Generate-vDisk.ps1
@@ -11,20 +11,32 @@ asnp Citrix.*
 
 # Defines Citrix Site Variables. Do not edit unless making a new script.
 
+# START EDITING
+
+# If creating or reconfiguring this script, only modify the below variables to customize to your Citrix environment and vDisk configurations.
+# If you don't need to label all of the settings, just comment some out. Example: #$vDiskOriginalFile.
+
+
 $StoreName = "STORE-NAME"
 $SiteName = "SITE-NAME"
 $PVSServerName = "Citrix-PVS-Server-Name"
-
-
-# Defines the Citrix PVS vDISK name. This can be changed although it will update as a string. DO NOT ADD FILE EXTENSION.
-
-$vDiskName = "vDISK-Name"
+$vDiskName = "vDISK-Name" # Defines the Citrix PVS vDISK name. DO NOT ADD FILE EXTENSION.
 $PVSUpdater = "Enter The PVS Updater Device Name Here."
 $vDiskSize = "Size-In-MB"
+$vDiskDescription = "ADD THE DESCRIPTION FOR VDISK IMAGE HERE"
+$vDiskDate = "Date" # Date Format Example: 3/24/2022 11:16:19 AM
+$vDiskAuthor = "Author Name"
+$vDiskTitle = "Title"
+$vDiskCompany = "Company Name"
+$vDiskInternalName = "Internal Name"
+$vDiskOriginalFile = "Original File Name"
+
 
 # Defines the name of the Device Collection for automation purposes. Change as needed. 
 
 $DeviceCollection = "Device Collection"
+
+# END EDITING 
 
 
 # Creates the new vDisk to the Citrix PVS Store. Modify SizeinMB to your requirements.
@@ -39,7 +51,7 @@ New-PVSDiskLocator -Name "$vDiskName" -StoreName "$StoreName" -ServerName "$PVSS
 
 # Adds details to the vDisk. Please customize as necessary; example - if you don't label company name or original file, just remove that parameter.
 
-Set-PVSDisk -Name "$vDiskName" -StoreName "$StoreName" -SiteName "$SiteName" -longDescription "ADD THE DESCRIPTION FOR VDISK IMAGE HERE" -date "CHANGE THE DATE HERE" -author "Jody Ingram (Change as needed)" -title "Title of Author if necessary." -company "Company if necessary" -internalName "InternalName if necessary" -originalFile "Image being replaced if you use this process"
+Set-PVSDisk -Name "$vDiskName" -StoreName "$StoreName" -SiteName "$SiteName" -longDescription "$vDiskDescription" -date "$vDiskDate" -author "$vDiskAuthor" -title "$vDiskTitle" -company "$vDiskCompany" -internalName "$vDiskInternalName" -originalFile "$vDiskOriginalFile"
 
 
 # Assigns the vDisk to a Device Collection
