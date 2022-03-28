a72c5c44340bfe3969d602d18a7963e500a4eb8c
Jody Ingram <22178885+Jody-Ingram@users.noreply.github.com>
2022-03-28 09:28:58 -0400

    Add files via upload

---
+++ Azure-CLI-Arc-Deploy-VM-to-Azure.sh
@@ -0,0 +1,17 @@
+# Script  :  Azure-SLI-Deploy-VM-via-ARC.sh
+# Version :  1.0
+# Date    :  5/1/2021
+# Author: Jody Ingram
+# Pre-reqs: N/A
+# Notes: Use this Azure CLI script to automatically deploy a VM using Azure Arc.
+
+New-AzResourceGroupDeployment `
+  -ResourceGroupName 'Name of Resource Group' `
+  -TemplateFile ~/AzureArcLinuxCustomScriptExtensions.json `
+  -TemplateParameterObject @{
+    TemplateFile = 'Specify Template File Name.json'
+    MachineName = 'Machine Name of ARC'
+    Location = 'Location of ARM VM'
+    WorkspaceId = 'Customer/Workspace ID'
+    WorkspaceKey = 'Customer/Workspace Key'
+  }
