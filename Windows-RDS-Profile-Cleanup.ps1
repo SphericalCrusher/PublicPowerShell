75d7f56ce7d92c2e896926c0aa1026f972c6c480
Jody Ingram <22178885+Jody-Ingram@users.noreply.github.com>
2024-01-18 14:26:27 -0500

    Update and rename Citrix-RDS-Profile-Cleanup.ps1 to Windows-RDS-Profile-Cleanup.ps1

--- Windows-RDS-Profile-Cleanup.ps1
+++ Windows-RDS-Profile-Cleanup.ps1
@@ -1,14 +1,14 @@
 <#
-Script  :  Citrix-RDS-Profile-Cleanup.ps1
+Script  :  Windows-RDS-Profile-Cleanup.ps1
 Version :  1.0
 Date    :  1/12/23
 Author: Jody Ingram
-Notes: This script automatically does a recursive cleanup of older Citrix user profiles on the Citrix profile servers or local VDAs.
+Notes: This script automatically does a recursive cleanup of older Windows or Citrix user profiles on the Citrix profile servers, local VDAs, or C:\Users directory of the machine.
 #>
 
 $ErrorActionPreference= 'silentlycontinue'
 $Users = Get-WmiObject -Class Win32_UserProfile
-$IgnoreList = "svc_ctxapp", "18576", "Default", "Public"
+$IgnoreList = "LocalAdmin", "Default", "Public"
 
 :OuterLoop
 foreach ($User in $Users) {
